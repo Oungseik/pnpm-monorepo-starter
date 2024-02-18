4dfa4b4cff0f60e534c29c87a8a87073670e4507
Oungseik <mhemaungthuwin@gmail.com>
2024-02-18 13:48:57 +0630

    feat: setup trpc package

---
+++ packages/trpc/trpc.ts
@@ -0,0 +1,26 @@
+import { TRPCError, initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import jwt from "jsonwebtoken";
+
+export const createContext = ({ req }: trpcExpress.CreateExpressContextOptions) => {
+  const token = req.headers.authorization?.slice(7);
+  return { token };
+};
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
+
+const t = initTRPC.context<Context>().create();
+
+export const router = t.router;
+
+export const publicProcedure = t.procedure;
+
+export const authProcedure = publicProcedure.use(async (opts) => {
+  const { token } = opts.ctx;
+
+  if (!token) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Authorization token not provided." });
+  }
+
+  return opts.next();
+});
