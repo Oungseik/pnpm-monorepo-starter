5de029106bc65657197c62fa3cfc6b99515b32ba
Oungseik <mhemaungthuwin@gmail.com>
2024-02-18 18:44:51 +0630

    feat: add central configuration package

---
+++ packages/config/index.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+  /** JSON web token specific configurations */
+  jwt: {
+    authSecret: process.env.AUTH_SECRET || "secret for your authentication token",
+    refreshSecret: process.env.REFRESH_SECRET || "secret for your refresh token",
+  },
+};
+
+export type Config = typeof config;
