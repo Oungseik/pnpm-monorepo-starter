5f3f6bf86583942082758c6b9a36eb845be360b3
Oungseik <mhemaungthuwin@gmail.com>
2024-02-18 13:58:34 +0630

    feat: read environment variables in config

--- packages/repo-config/index.ts
+++ packages/repo-config/index.ts
@@ -1,3 +1,10 @@
-export const config = {};
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+  /** JSON web token specific configurations */
+  jwt: {},
+};
 
 export type Config = typeof config;
