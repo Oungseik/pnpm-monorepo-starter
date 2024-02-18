24a0f7ec0cb90b55f2f535981e136a46e9a0d295
Oungseik <mhemaungthuwin@gmail.com>
2024-02-18 11:52:35 +0630

    feat: create package for prettier config

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  useTabs: false,
+  arrowParens: "always",
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  endOfLine: "lf",
+};
