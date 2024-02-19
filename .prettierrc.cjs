f0f965fcecef45e89087d06003532f0a45152644
Oungseik <mhemaungthuwin@gmail.com>
2024-02-19 10:55:14 +0630

    fix: remove svelte prettier plugin in the root config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,5 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   ...require("@repo/prettier-config"),
-  plugins: [import("prettier-plugin-svelte"), import("prettier-plugin-tailwindcss")],
-  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+  plugins: [import("prettier-plugin-tailwindcss")],
 };
