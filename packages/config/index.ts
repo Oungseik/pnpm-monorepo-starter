6a21a89923c3c95fc2aa96f229b56df6c621260e
Oungseik <mhemaungthuwin@gmail.com>
2024-02-20 17:44:56 +0630

    chore: bundle the package with tsup

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -8,6 +8,11 @@ export const config = {
     authSecret: process.env.AUTH_SECRET || "secret for your authentication token",
     refreshSecret: process.env.REFRESH_SECRET || "secret for your refresh token",
   },
+
+  /** config for apps/server */
+  server: {
+    port: process.env.SERVER_PORT || 8080,
+  },
 };
 
 export type Config = typeof config;
