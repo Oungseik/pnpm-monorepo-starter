137827d2cb38f928e9ad93a15ae5171e707aa150
Oungseik <mhemaungthuwin@gmail.com>
2024-02-22 12:59:36 +0630

    refactor: make the package decouple with trpc

--- packages/orm/src/lib/datasource/sqlite.ts
+++ packages/orm/src/lib/datasource/sqlite.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import { users } from "../schema/user.schema";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const sqlite = new Database(process.env.SQLITE_URL);
 
